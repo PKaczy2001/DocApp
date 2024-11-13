6e2aa4040cf150e8fc7f8169057e1d9040c82a35
Piotr Kaczmarczyk <sa22b012@technikum-wien.at>
2024-11-13 15:16:11 +0100

    Kalender Update 1; Termine können Hinzugefügt werde

--- DocApp.js
+++ DocApp.js
@@ -5,9 +5,41 @@ const DocApp = {
         return {
             page: "Home",
             calendar: "Lade Kalenderdaten...",
-            htmlContent: "" // Variable für den HTML-Inhalt
+            htmlContent: "", // Variable für den HTML-Inhalt
+            eventName: "",
+            eventStartTime: "",
+            eventEndTime: "",
+            eventDate:"", 
+            createEventMessage: ""
         };
     },
+    methods: {
+        async addEvent(evt){
+            evt.preventDefault(); // Verhindert das Seiten-Reload
+
+            console.log('gapi verfügbar?', typeof gapi !== 'undefined' ? 'Ja' : 'Nein');
+            const event ={
+                summary: this.eventName,
+                start: {
+                    dateTime: `${this.eventDate}T${this.eventStartTime}+01:00` //DAS START Datum Syntax: "2024-11-13T10:00:00"
+                },
+                end: {
+                    dateTime: `${this.eventDate}T${this.eventEndTime}+01:00` //DAS END Datum
+                }
+            };
+            try {
+                await gapi.client.calendar.events.insert({
+                    calendarId: "primary",
+                    resource: event
+                });
+                this.createEventMessage = "Erfolg!!!!!!!";
+            } catch(error){
+                console.error('FEHLER!!!!!!!!!!', error);
+                this.createEventMessage = "FEHLER!!!!!!!!!" + error.message;A
+                console.log('das ist START:', `${this.eventDate}T${this.eventEndTime}` );
+            }
+        }
+    }
 };
 
 // Erstelle und mounte die App
